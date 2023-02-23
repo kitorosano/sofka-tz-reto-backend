fc4bea7102d03b478cd3e6c5980d6646b419bdc8
kitorosano <rosanoesteban2@gmail.com>
2023-02-23 16:50:33 -0300

    fix: se arregla uso de mongo repository

--- src/main/java/uy/com/sofka/retodevjunior/SofkaRetoDevJuniorWebBackendApplication.java
+++ src/main/java/uy/com/sofka/retodevjunior/SofkaRetoDevJuniorWebBackendApplication.java
@@ -3,6 +3,7 @@ package uy.com.sofka.retodevjunior;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,7 +11,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
-@EnableReactiveMongoRepositories
+@EnableMongoRepositories
 public class SofkaRetoDevJuniorWebBackendApplication {
   
   @Bean
