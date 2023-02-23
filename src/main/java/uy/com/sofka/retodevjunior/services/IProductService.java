c8aaa140a091c0b4e5bdfa8361db28c9920bca3f
kitorosano <rosanoesteban2@gmail.com>
2023-02-23 16:51:04 -0300

    feat: se agrega service con nuevos metodos

--- src/main/java/uy/com/sofka/retodevjunior/services/IProductService.java
+++ src/main/java/uy/com/sofka/retodevjunior/services/IProductService.java
@@ -1,14 +1,18 @@
 package uy.com.sofka.retodevjunior.services;
 
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import org.springframework.data.domain.Pageable;
 import uy.com.sofka.retodevjunior.dtos.ProductDTO;
+import uy.com.sofka.retodevjunior.dtos.ProductListDTO;
+import java.util.Optional;
 
 public interface IProductService {
-  Mono<ProductDTO> save(ProductDTO dto);
-  Flux<ProductDTO> findAll();
-  Mono<ProductDTO> findById(String id);
-  Mono<ProductDTO> update(String id, ProductDTO dto);
-  Mono<Void> delete(String id);
-  Mono<Void> deleteAll();
+  ProductDTO save(ProductDTO dto);
+  ProductListDTO findAll(Pageable pageable);
+  Optional<ProductDTO> findById(String id);
+  ProductDTO update(String id, ProductDTO dto);
+  ProductDTO removeFromInventory(String id, Integer quantity);
+  ProductDTO addToInventory(String id, Integer quantity);
+  ProductDTO toggleEnabled(String id, Boolean enabled);
+  void delete(String id);
+  void deleteAll();
 }
